1db498bf45180f3c70986df89801dda898abfbf9
username <Jzazzi56@gmail.comn>
2023-11-18 16:15:37 -0800

    changed how the questions are fetched so you can fetch all avalible questions at random

--- src/components/TriviaCategories/MovieTrivia.jsx
+++ src/components/TriviaCategories/MovieTrivia.jsx
@@ -5,156 +5,51 @@ const MovieTrivia = () => {
   const [trivia, setTrivia] = useState("");
   const [triviaQuestion, setTriviaQuestion] = useState("");
   const [triviaAnswer, setTriviaAnswer] = useState("");
+  const [userInput, setUserInput] = useState("");
   const [score, setScore] = useState(0);
 
-  const handleMovies1 = async (e) => {
+  const fetchQuestions = async () => {
     try {
-      e.preventDefault();
       const res = await fetch("https://jservice.io/api/category?id=102");
       const data = await res.json();
       setTrivia(data);
       console.log(data);
 
-      const triviaQuestion = data.clues[1].question;
-      console.log(triviaQuestion);
-      setTriviaQuestion(triviaQuestion);
+      const randomeIndex = Math.floor(Math.random() * data.clues.length);
+      const currentQuestion = data.clues[randomeIndex].question;
+      const currentAnswer = data.clues[randomeIndex].answer;
+
+      setTriviaQuestion(currentQuestion);
+      setTriviaAnswer(currentAnswer);
     } catch (error) {
       console.log("failed to fetch question", error);
     }
   };
-  const handleMovies2 = async (e) => {
-    try {
-      e.preventDefault();
-      const res = await fetch("https://jservice.io/api/category?id=102");
-      const data = await res.json();
-      setTrivia(data);
-      console.log(data);
 
-      const triviaQuestion = data.clues[2].question;
-      console.log(triviaQuestion);
-      setTriviaQuestion(triviaQuestion);
-    } catch (error) {
-      console.log("failed to fetch question", error);
-    }
-  };
-  const handleMovies3 = async (e) => {
-    try {
-      e.preventDefault();
-      const res = await fetch("https://jservice.io/api/category?id=102");
-      const data = await res.json();
-      setTrivia(data);
-      console.log(data);
+  useEffect(() => {
+    fetchQuestions();
+  }, []);
 
-      const triviaQuestion = data.clues[3].question;
-      console.log(triviaQuestion);
-      setTriviaQuestion(triviaQuestion);
-    } catch (error) {
-      console.log("failed to fetch question", error);
-    }
-  };
-  const handleMovies4 = async (e) => {
-    try {
-      e.preventDefault();
-      const res = await fetch("https://jservice.io/api/category?id=102");
-      const data = await res.json();
-      setTrivia(data);
-      console.log(data);
-
-      const triviaQuestion = data.clues[4].question;
-      console.log(triviaQuestion);
-      setTriviaQuestion(triviaQuestion);
-    } catch (error) {
-      console.log("failed to fetch question", error);
-    }
-  };
-  const handleMovies5 = async (e) => {
-    try {
-      e.preventDefault();
-      const res = await fetch("https://jservice.io/api/category?id=102");
-      const data = await res.json();
-      setTrivia(data);
-      console.log(data);
-
-      const triviaQuestion = data.clues[5].question;
-      console.log(triviaQuestion);
-      setTriviaQuestion(triviaQuestion);
-    } catch (error) {
-      console.log("failed to fetch question", error);
-    }
-  };
-  const handleMovies6 = async (e) => {
-    try {
-      e.preventDefault();
-      const res = await fetch("https://jservice.io/api/category?id=102");
-      const data = await res.json();
-      setTrivia(data);
-      console.log(data);
-
-      const triviaQuestion = data.clues[6].question;
-      console.log(triviaQuestion);
-      setTriviaQuestion(triviaQuestion);
-    } catch (error) {
-      console.log("failed to fetch question", error);
-    }
-  };
-  const handleMovies7 = async (e) => {
-    try {
-      e.preventDefault();
-      const res = await fetch("https://jservice.io/api/category?id=102");
-      const data = await res.json();
-      setTrivia(data);
-      console.log(data);
-
-      const triviaQuestion = catOne.clues[7].question;
-      console.log(triviaQuestion);
-      setTriviaQuestion(triviaQuestion);
-    } catch (error) {
-      console.log("failed to fetch question", error);
-    }
-  };
-  const handleMovies8 = async (e) => {
-    try {
-      e.preventDefault();
-      const res = await fetch("https://jservice.io/api/category?id=102");
-      const data = await res.json();
-      setTrivia(data);
-      console.log(data);
-
-      const triviaQuestion = data.clues[8].question;
-      console.log(triviaQuestion);
-      setTriviaQuestion(triviaQuestion);
-    } catch (error) {
-      console.log("failed to fetch question", error);
-    }
-  };
-  const handleMovies9 = async (e) => {
-    try {
-      e.preventDefault();
-      const res = await fetch("https://jservice.io/api/category?id=102");
-      const data = await res.json();
-      setTrivia(data);
-      console.log(data);
-
-      const triviaQuestion = data.clues[9].question;
-      console.log(triviaQuestion);
-      setTriviaQuestion(triviaQuestion);
-    } catch (error) {
-      console.log("failed to fetch question", error);
-    }
+  const handleMovies = async () => {
+    const randomeIndex = Math.floor(Math.random() * trivia.clues.length);
+    const currentQuestion = trivia.clues[randomeIndex].question;
+    const currentAnswer = trivia.clues[randomeIndex].answer;
+    console.log(trivia);
+    setTriviaQuestion(currentQuestion);
+    setTriviaAnswer(currentAnswer);
   };
 
   const handleAnswer = async (e) => {
     e.preventDefault();
-    const res = await fetch("https://jservice.io/api/category?id=102");
-    const data = await res.json();
-    setTrivia(data);
 
-    if (triviaAnswer === data.clues[1].answer) {
-      console.log("correct");
+    if (userInput.trim().toLowerCase() === triviaAnswer.trim().toLowerCase()) {
       setScore(score + 100);
+
+      fetchQuestions();
     } else {
-      console.log("incorrect");
+      console.log("wrong answer");
     }
+    setUserInput("");
   };
 
   return (
@@ -165,32 +60,32 @@ const MovieTrivia = () => {
       </div>
 
       <div className="categoryContainer">
-        <button onClick={handleMovies1} className="board">
+        <button onClick={handleMovies} className="board">
           100
         </button>
-        <button onClick={handleMovies2} className="board">
+        <button onClick={handleMovies} className="board">
           200
         </button>
-        <button onClick={handleMovies3} className="board">
+        <button onClick={handleMovies} className="board">
           300
         </button>
 
-        <button onClick={handleMovies4} className="board">
+        <button onClick={handleMovies} className="board">
           400
         </button>
-        <button onClick={handleMovies5} className="board">
+        <button onClick={handleMovies} className="board">
           500
         </button>
-        <button onClick={handleMovies6} className="board">
+        <button onClick={handleMovies} className="board">
           600
         </button>
-        <button onClick={handleMovies7} className="board">
+        <button onClick={handleMovies} className="board">
           700
         </button>
-        <button onClick={handleMovies8} className="board">
+        <button onClick={handleMovies} className="board">
           800
         </button>
-        <button onClick={handleMovies9} className="board">
+        <button onClick={handleMovies} className="board">
           900
         </button>
       </div>
@@ -200,13 +95,22 @@ const MovieTrivia = () => {
       </div>
       <div className="flex justify-center mt-2">
         <form className="flex flex-col">
-          <input type="text" placeholder="Answer" className="input" />
-          <button onClick={handleAnswer} className="inputBtn">
-            Submit
-          </button>
+          <input
+            type="text"
+            placeholder="Answer"
+            className="input"
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+          />
+          <div>
+            <button onClick={handleAnswer} className="inputBtn">
+              Submit
+            </button>
+          </div>
         </form>
       </div>
-      <div className="flex justify-center mt-2">
+
+      <div className="flex mt-2 ml-2">
         <Footer />
       </div>
     </>
